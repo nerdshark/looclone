7a8f95d1a60bc3ba2f9bd2896ffeb8fc500516b3
Dustin Mays <dork.fish.wat@gmail.com>
2010-07-09 14:25:39 -0400

    added shell of player class

---
+++ player.rb
@@ -0,0 +1,26 @@
+module LooClone
+
+# Gosu/Chingu stuff
+require 'rubygems'
+require 'chingu'
+include Chingu
+include Gosu
+
+# LooClone stuff
+require 'states'
+
+class Player < Chingu::GameObject
+   def initialize
+
+   end
+
+   def update
+
+   end
+
+   def draw
+
+   end
+end
+
+end
