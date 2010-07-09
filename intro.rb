49bf7b7f499ef5049cec8e7da2070fe5469cf5e9
Dustin Mays <dork.fish.wat@gmail.com>
2010-07-09 13:43:37 -0400

    added text with instructions on bypassing the intro

--- intro.rb
+++ intro.rb
@@ -14,6 +14,11 @@ class IntroState < Chingu::GameState
       super
       self.input = { :esc => Proc.new { switch_game_state( MainMenuState.new ) } }
       $window.caption = "LooClone Intro"
+      @mytext = Chingu::Text.create("Press esc to continue", :x => 512, :y => 300)
+   end
+      
+   def draw
+      @mytext.draw
    end
 end
 
